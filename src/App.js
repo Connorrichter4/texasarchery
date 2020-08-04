7a91fd65e795a5d317474bb39d48abda57c995af
Connorrichter4 <Connorrichter4@gmail.com>
2020-08-04 12:40:25 -0500

    starting to create the contact us page and home page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import {Link, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>
-          TexasArchery
-        </h1></header>
-    </div>
-  );
+		<div className='App'>
+      <h1>Texas Archery Academy</h1>
+			<Link to='/'>HOME</Link>
+			<Link to='/firstvisit'>FIRST VISIT FAQ</Link>
+			<Link to='/classes'>CLASSES</Link>
+			<Link to='/events'>GROUPS & BIRTHDAYS</Link>
+			<Link to='/locations'>LOCATIONS</Link>
+		</div>
+	);
 }
 
 export default App;
