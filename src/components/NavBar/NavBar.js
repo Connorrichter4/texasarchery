d87b6b07ebd37cc3dd8ed193cae393f69efba6b5
Connorrichter4 <Connorrichter4@gmail.com>
2020-08-07 13:37:02 -0500

    updated navbar components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,13 +3,18 @@ import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import DehazeIcon from '@material-ui/icons/Dehaze';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import './NavBar.css';
 
+const useStyles = makeStyles({
+	paper: {
+		background: 'gray',
+	},
+});
+
 function NavBar() {
+	const classes = useStyles();
 	const [state, setState] = useState({
-		top: false,
-		left: false,
-		bottom: false,
 		right: false,
 	});
 
@@ -26,22 +31,35 @@ function NavBar() {
 
 	return (
 		<div className='header'>
-            <h1>Texas Archery Academy</h1>
+			<h1 className='title'>Texas Archery Academy</h1>
 			<React.Fragment>
 				<Button onClick={toggleDrawer('right', true)}>
-					<DehazeIcon fontSize='large' />
+					<DehazeIcon  style={{ fill: 'white'}} />
 				</Button>
 				<Drawer
 					anchor='right'
 					open={state['right']}
 					onClose={toggleDrawer('right', false)}
-					onClick={toggleDrawer('right', false)}>
-					<Link to='/'>HOME</Link>
-					<Link to='/firstvisit'>FIRST VISIT FAQ</Link>
-					<Link to='/classes'>CLASSES</Link>
-					<Link to='/events'>GROUPS & BIRTHDAYS</Link>
-					<Link to='/locations'>LOCATIONS</Link>
-					<Link to='/contact'>CONTACT US</Link>
+					onClick={toggleDrawer('right', false)}
+					classes={{ paper: classes.paper }}>
+					<Link to='/' className='nav-link'>
+						HOME
+					</Link>
+					<Link to='/firstvisit' className='nav-link'>
+						FIRST VISIT FAQ
+					</Link>
+					<Link to='/classes' className='nav-link'>
+						CLASSES
+					</Link>
+					<Link to='/events' className='nav-link'>
+						GROUPS & BIRTHDAYS
+					</Link>
+					<Link to='/locations' className='nav-link'>
+						LOCATIONS
+					</Link>
+					<Link to='/contact' className='nav-link'>
+						CONTACT US
+					</Link>
 				</Drawer>
 			</React.Fragment>
 		</div>
