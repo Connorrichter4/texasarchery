d6f6ef760cf7ad6b0a2fd13a9a70a13df8cab1ff
Connorrichter4 <Connorrichter4@gmail.com>
2020-08-19 11:05:35 -0400

    added footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 import Contact from './components/Contact/Contact';
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
 				<Route path='/' exact component={Home} />
 				<Route path='/contact' exact component={Contact} />
 			</Switch>
+			<Route path='*' component={Footer} />
 		</div>
 	);
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+			<footer>
+				<div>
+					<p>Copyright &copy; 2020 TexasArchery.info - All Rights Reserved</p>
+				</div>
+				<div>
+					<h3>Hours of Operation</h3>
+					<p>Monday - Friday 3:00pm - 8:00pm</p>
+					<p>Saturday - Sunday 9:00am - 6:00pm</p>
+				</div>
+				<div>
+					<p>Staff Login</p>
+				</div>
+			</footer>
+		);
+}
+
+export default Footer;
