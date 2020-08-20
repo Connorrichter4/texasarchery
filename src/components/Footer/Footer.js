e999ee7ab472a74decb673b5fb97a7faa1e8883e
Connorrichter4 <Connorrichter4@gmail.com>
2020-08-20 10:14:07 -0400

    working on updating footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,16 @@ import './Footer.css';
 
 function Footer() {
     return (
-			<footer>
+			<footer className='footer'>
 				<div>
+					<p>Staff Login</p>
+					<p>Contact Us</p>
 					<p>Copyright &copy; 2020 TexasArchery.info - All Rights Reserved</p>
 				</div>
 				<div>
 					<h3>Hours of Operation</h3>
 					<p>Monday - Friday 3:00pm - 8:00pm</p>
 					<p>Saturday - Sunday 9:00am - 6:00pm</p>
-				</div>
-				<div>
-					<p>Staff Login</p>
 				</div>
 			</footer>
 		);
