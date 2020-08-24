a537875a20f64050aad21d9120a70425dd7fd788
Connorrichter4 <Connorrichter4@gmail.com>
2020-08-24 18:40:41 -0500

    working on functionality of contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -26,6 +26,17 @@ const useStyles = makeStyles((theme) => ({
 function Contact() {
 	const classes = useStyles();
 
+	const [contact, setContact] = useState({
+		name: '',
+		email: '',
+		message: ''
+	})
+
+	const handleChange = (event) => {
+		event.persist();
+		setContact({...contact, [event.target.name]: event.target.value});
+	}
+
 	return (
 		<div className='contact-us'>
 			<form className={classes.root} noValidate autoComplete='off'>
@@ -33,27 +44,34 @@ function Contact() {
 				<TextField
 					id='filled-basic'
 					label='Name'
+					name='name'
 					variant='filled'
 					required
 					style={{ margin: 8 }}
 					margin='normal'
 					fullWidth
+					onChange={handleChange}
+					
 				/>
 				<TextField
 					id='filled-basic'
 					label='Email'
+					name='email'
 					variant='filled'
 					required
 					fullWidth
+					onChange={handleChange}
 				/>
 				<TextField
 					id='filled-basic'
 					label='Message'
+					name='message'
 					variant='filled'
 					required
 					multiline
 					rows={4}
 					fullWidth
+					onChange={handleChange}
 				/>
 				<Button>Submit</Button>
 			</form>
