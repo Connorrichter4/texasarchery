6a1c34d730b3f8cb14ff7809155e72fcccbdf34c
Connorrichter4 <Connorrichter4@gmail.com>
2020-09-28 14:53:52 -0500

    working on netlify form submission

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import './Contact.css';
 
 const useStyles = makeStyles((theme) => ({
