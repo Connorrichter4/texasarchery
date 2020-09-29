22ea72b50ae688f5a07d15ed50dffdb56df24d60
Connorrichter4 <Connorrichter4@gmail.com>
2020-09-28 19:10:30 -0500

    working on netlify form with react;

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,114 +1,75 @@
-import React, { useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-// import TextField from '@material-ui/core/TextField';
-// import Button from '@material-ui/core/Button';
+import React, { useState, useEffect } from 'react';
+
 import './Contact.css';
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		'& > *': {
-			margin: theme.spacing(1),
-		},
-		display: 'flex',
-		flexDirection: 'column',
-		maxWidth: '600px',
-		margin: '1rem auto',
-		padding: '1rem',
-		height: '65vh',
-		justifyContent: 'center',
-		alignItems: 'center',
-		background: '#fffcf2',
-		borderRadius: '5px',
-		boxShadow: '5px 5px 20px black',
-	},
-}));
+const encode = (data) => {
+	return Object.keys(data)
+		.map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+		.join('&');
+};
 
 function Contact() {
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
 
-	const classes = useStyles();
-
-	// const [contact, setContact] = useState({
-	// 	name: '',
-	// 	email: '',
-	// 	message: '',
-	// });
+	const [contact, setContact] = useState({
+		name: '',
+		email: '',
+		message: '',
+	});
 
 	// const [emailSent, setEmailSent] = useState(false);
 
-	// const handleChange = (event) => {
-	// 	event.persist();
-	// 	setContact({ ...contact, [event.target.name]: event.target.value });
-	// };
+	const handleChange = (event) => {
+		event.persist();
+		setContact({ ...contact, [event.target.name]: event.target.value });
+	};
 
-	// const handleSubmit = (event) => {
-	// 	event.preventDefault();
-	// };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: encode({ 'form-name': 'contact', ...contact }),
+		})
+			.then(() => alert('Success!'))
+			.catch((error) => alert(error));
+	};
 
 	return (
 		<div className='contact-us'>
 			<form
-				className={classes.root}
-				noValidate
-				autoComplete='off'
-				// onSubmit={handleSubmit}
+				className='contact-form'
+				onSubmit={handleSubmit}
 				method='POST'
-				action='/'
 				name='contact'>
 				<input type='hidden' name='form-name' value='contact' />
 				<h2>Contact Us:</h2>
-				{/* <TextField
-					id='filled-basic'
-					label='Name'
-					name='name'
-					variant='filled'
-					required
-					style={{ margin: 8 }}
-					margin='normal'
-					fullWidth
-					// onChange={handleChange}
-					// value={contact.name}
-				/>
-				<TextField
-					id='filled-basic'
-					label='Email'
-					name='email'
-					variant='filled'
-					required
-					fullWidth
-					// onChange={handleChange}
-					// value={contact.email}
-				/>
-				<TextField
-					id='filled-basic'
-					label='Message'
-					name='message'
-					variant='filled'
-					required
-					multiline
-					rows={4}
-					fullWidth
-					// onChange={handleChange}
-					// value={contact.message}
-				/>
-
-				<Button type='submit'>Submit</Button> */}
 				<p>
-					<label>
-						Name: <input type='text' name='name' />
-					</label>
+					<label>Name:</label>
+					<input
+						type='text'
+						name='name'
+						onChange={handleChange}
+						value={contact.name}
+					/>
 				</p>
 				<p>
-					<label>
-						Email: <input type='email' name='email' />
-					</label>
+					<label>Email:</label>
+					<input
+						type='email'
+						name='email'
+						onChange={handleChange}
+						value={contact.email}
+					/>
 				</p>
 				<p>
-					<label>
-						Message: <textarea name='message'></textarea>
-					</label>
+					<label>Message:</label>
+					<textarea
+						name='message'
+						onChange={handleChange}
+						value={contact.message}></textarea>
 				</p>
 				<p>
 					<button type='submit'>Send</button>
